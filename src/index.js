27270cbc348b95bde7f458b26f5289961fda0505
Gianluca Esposito <g@esposi.to>
2017-05-29 13:45:48 +0200

    Add stats prefixes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import StatsD from 'statsd-client';
 import { github, discourse, mailup } from './services';
 
 const { STATSD_HOST, STATSD_PORT, STATSD_PREFIX } = process.env;
+const { GITHUB_PREFIX, DISCOURSE_PREFIX, MAILUP_PREFIX } = process.env;
 const { NODE_ENV } = process.env;
 
 const statsd = new StatsD({
@@ -18,19 +19,19 @@ Promise.all([
   github.getReposData()
     .then((data) => {
       Object.keys(data).map((key) => {
-        statsd.gauge(key, data[key]);
+        statsd.gauge(`${GITHUB_PREFIX}${key}`, data[key]);
       });
     }),
   discourse.getForumData()
     .then((data) => {
       Object.keys(data).map((key) => {
-        statsd.gauge(key, data[key]);
+        statsd.gauge(`${DISCOURSE_PREFIX}${key}`, data[key]);
       });
     }),
   mailup.getMailData()
     .then((data) => {
       Object.keys(data).map((key) => {
-        statsd.gauge(key, data[key]);
+        statsd.gauge(`${MAILUP_PREFIX}${key}`, data[key]);
       });
     }),
 ])
