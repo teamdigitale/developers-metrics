32eb6c7642ab7a205818c9503277acc17f295f32
Gianluca Esposito <g@esposi.to>
2018-06-30 10:13:20 +0200

    Update Docker configuration

--- docker-run.dev.sh
+++ docker-run.dev.sh
@@ -1,9 +1,10 @@
 # https://github.com/samuelebistoletti/docker-statsd-influxdb-grafana
 
-docker run -d \
+docker run --ulimit nofile=66000:66000 \
+  -d \
   --name docker-statsd-influxdb-grafana \
   -p 3003:3003 \
-  -p 3004:8083 \
+  -p 3004:8888 \
   -p 8086:8086 \
   -p 22022:22 \
   -p 8125:8125/udp \
