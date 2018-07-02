276ddaebb9ca1b4dfc54fd3af3fa5400c9e2707b
Gianluca Esposito <g@esposi.to>
2018-07-02 19:54:47 +0200

    Update GitHub service

--- src/server.js
+++ src/server.js
@@ -1,51 +1,55 @@
-require('dotenv').config();
+require("dotenv").config();
 
-import express from 'express';
-import request from 'request-promise-native';
-import querystring from 'querystring';
-import fs from 'fs';
+import express from "express";
+import request from "request-promise-native";
+import querystring from "querystring";
+import fs from "fs";
 
-import { mailupAuth, getAccessRefreshToken } from './services/mailup';
-import { githubAuth, getAccessToken } from './services/github';
+import { mailupAuth, getAccessRefreshToken } from "./services/mailup";
+import { githubAuth, getAccessToken } from "./services/github";
 
 const { SERVER_PORT } = process.env;
 
 const app = express();
 
-app.get('/mailup', (req, res) => {
-  return getAccessRefreshToken({ 'code': req.query.code })
-    .then((data) => {
+app.get("/mailup", (req, res) => {
+  return getAccessRefreshToken({ code: req.query.code })
+    .then(data => {
       const message = {
-        'message': 'paste these codes into `MAILUP_ACCESS_TOKEN` and `MAILUP_REFRESH_TOKEN` in your .env file'
+        message:
+          "paste these codes into `MAILUP_ACCESS_TOKEN` and `MAILUP_REFRESH_TOKEN` in your .env file"
       };
       const response = Object.assign(data, message);
       res.send(response);
 
       return response.message;
     })
-    .then((message) => {
+    .then(message => {
       // Exits the local process
       mailupAuth.reject(message);
     });
 });
 
-app.get('/github', (req, res) => {
-  return getAccessToken({ 'code': req.query.code })
-    .then((data) => {
+app.get("/github", (req, res) => {
+  return getAccessToken({ code: req.query.code })
+    .then(data => {
       const message = {
-        'message': 'paste this code into `GITHUB_ACCESS_TOKEN` in your .env file'
+        message: "paste this code into `GITHUB_ACCESS_TOKEN` in your .env file"
       };
       const response = Object.assign(data, message);
       res.send(response);
 
-      return response.message;
+      return {
+        message: response.message,
+        access_token: response.access_token
+      };
     })
-    .then((message) => {
+    .then(({ message, access_token }) => {
       // Exits the local process
-      githubAuth.reject(message);
+      githubAuth.reject(`${message}: ${access_token}`);
     });
 });
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}!`)
+  console.log(`Server listening on port ${SERVER_PORT}!`);
 });
--- src/services/github.js
+++ src/services/github.js
@@ -1,40 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
 
-import GitHub from 'github-api';
-import Requestable from 'github-api/dist/components/Requestable';
-import request from 'request-promise-native';
-import querystring from 'querystring';
-import opn from 'opn';
+import request from "request-promise-native";
+import querystring from "querystring";
+import opn from "opn";
+
+import GitHub from "github-api";
 
 const { GITHUB_URL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 const { GITHUB_ACCESS_TOKEN, GITHUB_USER } = process.env;
 const { SERVER_URL, SERVER_PORT, REFRESH_TOKEN_PATH } = process.env;
 
-const AUTH_URL  = 'login/oauth/authorize';
-const TOKEN_URL = 'login/oauth/access_token';
+const AUTH_URL = "login/oauth/authorize";
+const TOKEN_URL = "login/oauth/access_token";
 
 const token = {
-  'token': GITHUB_ACCESS_TOKEN,
-}
+  token: GITHUB_ACCESS_TOKEN
+};
 
-const github      = new GitHub(token);
-const requestable = new Requestable(token);
+const github = new GitHub(token);
 
-const githubAuth  = Promise.defer();
+const githubAuth = Promise.defer();
 
 function checkAuth(endpoint = AUTH_URL) {
   if (!GITHUB_ACCESS_TOKEN) {
     // No token/refresh pair is found in .env
     const qs = querystring.stringify({
-      'client_id'     : GITHUB_CLIENT_ID,
-      'redirect_uri'  : `${SERVER_URL}/github`,
-      'scope'         : '',
+      client_id: GITHUB_CLIENT_ID,
+      redirect_uri: `${SERVER_URL}/github`,
+      scope: ""
     });
 
     // Try to open into a Browser
     opn(`${GITHUB_URL}/${endpoint}?${qs}`);
 
-    console.info('Visit this URL in order to obtain your access token:');
+    console.info("Visit this URL in order to obtain your access token:");
     console.info(`${GITHUB_URL}/${endpoint}?${qs}`);
 
     return githubAuth.promise;
@@ -46,14 +45,14 @@ function checkAuth(endpoint = AUTH_URL) {
 
 function getAccessToken({ code }) {
   return request({
-    'method'          : 'POST',
-    'url'             : `${GITHUB_URL}/${TOKEN_URL}`,
-    'json'            : true,
-    'form': {
-      'client_id'     : GITHUB_CLIENT_ID,
-      'client_secret' : GITHUB_CLIENT_SECRET,
-      'redirect_uri'  : `${SERVER_URL}/github`,
-      'code'          : code,
+    method: "POST",
+    url: `${GITHUB_URL}/${TOKEN_URL}`,
+    json: true,
+    form: {
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      redirect_uri: `${SERVER_URL}/github`,
+      code: code
     }
   });
 }
@@ -63,41 +62,42 @@ function getReposData() {
 }
 
 function getReposDataWithToken() {
-  return github.getUser(GITHUB_USER).listRepos()
+  return github
+    .getUser(GITHUB_USER)
+    .listRepos()
+    .then(({ data: repos }) => repos)
     .then(getReposDetails);
 }
 
-function getReposDetails({ data }) {
-  const contributors  = [];
-  const branches      = [];
-  const commits       = [];
-  const forks         = [];
-  const pullRequests  = [];
-  const tags          = [];
+function getReposDetails(repos) {
+  const contributors = [];
+  const branches = [];
+  const commits = [];
+  const forks = [];
+  const pullRequests = [];
+  const tags = [];
 
-  (data).map((repo) => {
-    const getRepo   = github.getRepo(GITHUB_USER, repo.name);
-    const fullname  = `${GITHUB_USER}/${repo.name}`;
+  repos.map(repo => {
+    const repository = github.getRepo(GITHUB_USER, repo.name);
+    const fullname = `${GITHUB_USER}/${repo.name}`;
 
-    contributors.push(requestable._requestAllPages(`/repos/${fullname}/contributors`));
-    branches.push(requestable._requestAllPages(`/repos/${fullname}/branches`));
-    commits.push(getRepo.listCommits());
-    // commits.push(requestable._requestAllPages(`/repos/${fullname}/commits`));
-    forks.push(requestable._requestAllPages(`/repos/${fullname}/forks`));
-    pullRequests.push(requestable._requestAllPages(`/repos/${fullname}/pulls`));
-    // tags.push(requestable._requestAllPages(`/repos/${fullname}/tags`));
+    contributors.push(repository.getContributors());
+    branches.push(repository.listBranches());
+    commits.push(repository.listCommits());
+    forks.push(repository.listForks());
+    pullRequests.push(repository.listPullRequests());
+    tags.push(repository.listTags());
   });
 
   return Promise.all([
-    data,
+    repos,
     Promise.all(contributors),
     Promise.all(branches),
     Promise.all(commits),
     Promise.all(forks),
     Promise.all(pullRequests),
     Promise.all(tags)
-  ])
-    .then(getReposAggregateData);
+  ]).then(getReposAggregateData);
 }
 
 function getReposAggregateData([
@@ -107,25 +107,26 @@ function getReposAggregateData([
   commits,
   forks,
   pullRequests,
-  tags
+  tags,
+  gql
 ]) {
   return {
-    'repos'         : repos.length,
-    'contributors'  : contributors.reduce(accumulateDataLength, 0),
-    'branches'      : branches.reduce(accumulateDataLength, 0),
-    'commits'       : commits.reduce(accumulateDataLength, 0),
-    'forks'         : forks.reduce(accumulateDataLength, 0),
-    'pullRequests'  : pullRequests.reduce(accumulateDataLength, 0),
-    // 'tags'          : tags.reduce(accumulateDataLength, 0),
-  }
+    repos: repos.length,
+    contributors: contributors.reduce(accumulateDataLength, 0),
+    branches: branches.reduce(accumulateDataLength, 0),
+    commits: commits.reduce(accumulateDataLength, 0),
+    forks: forks.reduce(accumulateDataLength, 0),
+    pullRequests: pullRequests.reduce(accumulateDataLength, 0),
+    tags: tags.reduce(accumulateDataLength, 0)
+  };
 }
 
 function accumulateDataLength(acc, object) {
-  return (acc + object.data.length);
+  return acc + (object && object.data ? object.data.length : 0);
 }
 
 module.exports = {
   githubAuth,
   getAccessToken,
-  getReposData,
+  getReposData
 };
